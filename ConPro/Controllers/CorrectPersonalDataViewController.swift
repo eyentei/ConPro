4c57baccee2ddef608dc727d215c63e29459e9ac
Igor Kuznetsov <igor@macbook-pro-igor.local>
2018-05-26 06:45:07 +0300

    Корректно добавлены классы на экраны Профиля и Персональных данных

---
+++ ConPro/Controllers/CorrectPersonalDataViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CorrectPersonalDataViewController.swift
+//  ConPro
+//
+//  Created by Игорь on 26.05.2018.
+//  Copyright © 2018 ConPro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class CorrectPersonalDataViewController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ConPro/Controllers/CorrectProfileTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CorrectProfileTableViewController.swift
+//  ConPro
+//
+//  Created by Игорь on 26.05.2018.
+//  Copyright © 2018 ConPro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CorrectProfileTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
